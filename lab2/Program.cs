b99adc8f1a68d6b25190c0abb55233592fd142cc
Roman Urban-Orban <roman.urban.orban@gmail.com>
2023-11-02 23:25:38 +0200

    some more changes

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -7,61 +7,86 @@ namespace GeometricalShapes
     public interface IShape
     {
         double Area();
-        void ShowDetails();
+        double Perimeter();
+        string? Color { get; set; }
+        void ShowProperties();
     }
 
     // Abstract class
     public abstract class Shape : IShape
     {
         public abstract double Area();
-        public virtual void ShowDetails()
+        public abstract double Perimeter();
+        public string? Color { get; set; }
+        public virtual void ShowProperties()
         {
             Console.WriteLine("This is a shape");
         }
     }
 
-    // Square class
     public class Square : Shape
-    {
-        public double SideLength { get; set; }
+	{
+		private double _sideLength;
+		public double SideLength
+		{
+			get { return _sideLength; }
+			set
+			{
+				if (value <= 0)
+				{
+					throw new ArgumentException("Side length must be greater than zero.");
+				}
+				_sideLength = value;
+			}
+		}
 
         public Square(double sideLength)
         {
             SideLength = sideLength;
         }
 
-        public override double Area()
+		public override double Area()
         {
             return SideLength * SideLength;
         }
 
-        public override void ShowDetails()
+        public override double Perimeter()
         {
-            Console.WriteLine($"Square with side length: {SideLength}, area: {Area()}");
+            return 4 * SideLength;
         }
-    }
 
-    // Rectangle class
-    public class Rectangle : Shape
+        public override void ShowProperties()
+        {
+            Console.WriteLine($"Square: Side Length = {SideLength}, Area = {Area()}, Perimeter = {Perimeter()}, Color = {Color}");
+        }
+	}
+
+    public class Rectangle : Square
     {
         public double Width { get; set; }
-        public double Height { get; set; }
 
-        public Rectangle(double width, double height)
+        // call the base class constructor and pass sideLength to it
+        public Rectangle(double sideLength, double width) : base(sideLength)
         {
-            Width = width;
-            Height = height;
+            this.Width = width;
         }
 
         public override double Area()
         {
-            return Width * Height;
+            return SideLength * Width;
         }
 
-        public override void ShowDetails()
+        public override double Perimeter()
         {
-            Console.WriteLine($"Rectangle with width: {Width}, height: {Height}, area: {Area()}");
+            return 2 * (SideLength + Width);
         }
+
+        public override void ShowProperties()
+        {
+            Console.WriteLine($"Rectangle: Side Length = {SideLength}, Width = {Width}, Area = {this.Area()}, Perimeter = {this.Perimeter()}, Aspect Ratio = {Math.Round(AspectRatio, 2)}, Color = {Color}");
+        }
+
+        public double AspectRatio => Width / SideLength;
     }
 
     class Program
@@ -89,7 +114,7 @@ namespace GeometricalShapes
             // Display details
             foreach (Shape shape in shapes)
             {
-                shape.ShowDetails();
+                shape.ShowProperties();
             }
         }
     }
