d4c4c17ff662c8020ed847edc3fb61f6b80ac9e9
Roman Urban-Orban <roman.urban.orban@gmail.com>
2023-11-10 23:55:35 +0200

    lab2 adjustments

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace GeometricalShapes
 {
-    // Interface
+    // interface
     public interface IShape
     {
         double Area();
@@ -12,7 +12,7 @@ namespace GeometricalShapes
         void ShowProperties();
     }
 
-    // Abstract class
+    // abstract class
     public abstract class Shape : IShape
     {
         public abstract double Area();
@@ -40,9 +40,10 @@ namespace GeometricalShapes
 			}
 		}
 
-        public Square(double sideLength)
+        public Square(double sideLength, string color = "Gray")
         {
             SideLength = sideLength;
+            Color = color;
         }
 
 		public override double Area()
@@ -66,9 +67,10 @@ namespace GeometricalShapes
         public double Width { get; set; }
 
         // call the base class constructor and pass sideLength to it
-        public Rectangle(double sideLength, double width) : base(sideLength)
+        public Rectangle(double sideLength, double width, string color = "Gray") : base(sideLength)
         {
             this.Width = width;
+            Color = color;
         }
 
         public override double Area()
@@ -89,6 +91,17 @@ namespace GeometricalShapes
         public double AspectRatio => Width / SideLength;
     }
 
+    public static class ColorUtility
+    {
+        private static Random rand = new Random();
+
+        public static string GetRandomColor()
+        {
+            string[] colors = { "Red", "Green", "Blue", "Yellow", "Brown", "Orange", "Magenta", "Cyan", "White" };
+            return colors[rand.Next(colors.Length)];
+        }
+    }   
+
     class Program
     {
         static void Main(string[] args)
@@ -96,19 +109,36 @@ namespace GeometricalShapes
             List<Shape> shapes = new List<Shape>();
             Random rand = new Random();
 
-            // Adding random squares
+            // adding random squares
             for (int i = 0; i < 5; i++)
             {
-                double sideLength = rand.Next(1, 101); // Random length between 1 and 100
-                shapes.Add(new Square(sideLength));
+                double sideLength = rand.Next(1, 101); // random length between 1 and 100
+                if (i % 2 == 0) // for even iterations generate random color
+                {
+                    string color = ColorUtility.GetRandomColor();
+                    shapes.Add(new Square(sideLength, color));
+                }
+                else
+                {
+                    shapes.Add(new Square(sideLength)); // default color
+                }
             }
 
-            // Adding random rectangles
+            // adding random rectangles
             for (int i = 0; i < 5; i++)
             {
-                double width = rand.Next(1, 101); // Random width between 1 and 100
-                double height = rand.Next(1, 101); // Random height between 1 and 100
-                shapes.Add(new Rectangle(width, height));
+                double width = rand.Next(1, 101); // random width between 1 and 100
+                double height = rand.Next(1, 101); // random height between 1 and 100
+                if (i % 2 == 0) // for even iterations generate random color
+                {
+                    string color = ColorUtility.GetRandomColor();
+                    shapes.Add(new Rectangle(width, height, color));
+                }
+                else
+                {
+                    shapes.Add(new Rectangle(width, height)); // default color
+                }
+                
             }
 
             // Display details
